078aab205014da9cde3da70c45afafc226663474
Michael C. Iyke <ymichaelc@gmail.com>
2020-12-15 07:53:05 +0100

    Digression: express.static will serve even if the public folder isn't created yet

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const {log} = console;
 
 const express = require("express");
+const morgan = require("morgan");
 const http = require("http");
 const HOSTNAME = "127.0.0.1";
 const PORT = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((request, response, next) =>{
-  console.log(request.headers);
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/html");
   response.end(`
